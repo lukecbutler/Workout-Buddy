a5f72c99f9d29c4b316af16b8aedaf93e3587fc8
tarheelfan200 <tarheelfan3951@gmail.com>
2025-03-07 23:03:16 -0500

    push login - stable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+from flask_login import 
 import sqlite3
 
 # Initilize Flask App
@@ -6,15 +7,14 @@ app = Flask(__name__)
 
 DATABASE = "workouts.db"
 
-def getDbConnection():
-    connection = sqlite3.connect(DATABASE)
-    connection.row_factory = sqlite3.Row
-    return connection
-
 @app.route("/")
 def home():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
     return render_template("homepage.html")
 
 # Main function
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80, host='0.0.0.0')
